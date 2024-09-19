79c53dee640c51b86376dbc51fccce8e5d1db368
EggsyOnCode <farzansaqib2003@gmail.com>
2024-09-20 02:03:58 +0500

    staking contract: account models for staking manger created

--- programs/staking_solana/src/lib.rs
+++ programs/staking_solana/src/lib.rs
@@ -1,8 +1,9 @@
 use anchor_lang::prelude::*;
 use anchor_spl::{
     token::{TokenAccount, Mint,Token},
-    mint::*
+    associated_token::AssociatedToken
 };
+use std::mem::size_of;
 
 declare_id!("FQFDBABauvXLzYeGxFGHniNpEUM4NAGZHvKCnY8WjwJp");
 
@@ -16,12 +17,13 @@ pub mod constants {
 pub mod staking_solana {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
         Ok(())
     }
 
 
-    pub fn stake(ctx: Context<Initialize>, amt:u8) -> Result<()> {
+    pub fn stake(ctx: Context<Stake>, amt:u64) -> Result<()> {
+
         Ok(())
     }
 
@@ -50,7 +52,52 @@ pub struct Initialize<'info> {
 
     #[account()]
     pub mint: Account<'info, Mint>,
-
     pub token_program: Program<'info, Token>,
     pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Stake<'info> {
+    #[account(mut)]
+    pub signer : Signer<'info>,
+
+    #[account(
+        init_if_needed,
+        seeds = [constants::STAKE_INFO_ACCOUNT, signer.key.as_ref()],
+        space = size_of::<StakeInfo>() + 8,
+        bump,
+        payer = signer,
+    )]
+    pub stake_info_account : Account<'info, StakeInfo>,
+
+    #[account(
+        init_if_needed,
+        seeds = [constants::TOKEN_SEED, signer.key.as_ref()],
+        bump,
+        payer = signer,
+        token::mint = mint,
+        token::authority = user_staking_account
+    )]
+    pub user_staking_account : Account<'info, TokenAccount>,
+
+    #[account(
+        mut,
+        associated_token::mint = mint,
+        associated_token::authority = signer
+    )]
+    pub user_token_account : Account<'info, TokenAccount>,
+
+    #[account()]
+    pub mint: Account<'info, Mint>,
+    pub token_program: Program<'info, Token>,
+    pub associated_program : Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct StakeInfo {
+    pub total_staked: u64,
+    pub remaining_rewards: u64,
+    pub stake_at_slot: u64,
+    pub is_staked: bool,
 }
